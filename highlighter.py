159b1ab9d1f1e16aff9587b0b2ddc7af947ff617
Jason Christopher <jason.christopher@u.nus.edu>
2022-12-25 01:07:00 +0800

    Cleanup Code

--- highlighter.py
+++ highlighter.py
@@ -5,28 +5,30 @@ import os
 import argparse
 import re
 import fitz
+import shutil
 
 def main():
-    # Parsing command line arguments entered by user
-    args = parse_args()
-    # If File Path
-    if os.path.isfile(args['input_path']):
-        # Extracting File Info
-        extract_info(input_file=args['input_path'])
-        # Process a file
-        process_file(
-            input_file=args['input_path'], output_file=args['output_file'], 
-            search_str=args['search_str'] if 'search_str' in (args.keys()) else None, 
-            pages=args['pages'], action=args['action']
-        )
-    # If Folder Path
-    elif os.path.isdir(args['input_path']):
-        # Process a folder
-        process_folder(
-            input_folder=args['input_path'], 
-            search_str=args['search_str'] if 'search_str' in (args.keys()) else None, 
-            action=args['action'], pages=args['pages'], recursive=args['recursive']
-        )
+
+    print("##############################################")
+    print("Hi there! Please specify the file path")
+    while True:
+        path = input("Path: ")
+        if os.path.isfile(path):
+            break
+        print("Invalid path provided! Please provide a correct file path:")
+    print("Please specify an optional name to be appended to the new file")
+    opt_name = input("Name: ")
+    dest = path.strip()[:len(path) - 4] + opt_name + ".pdf"
+    # if not os.path.isfile(path):
+    shutil.copyfile(path, dest)
+    print("Please specify notes to be highlighted, separated by spaces")
+    notes = input("Notes: ")
+    notes = notes.strip().split()
+    print("Processing: ------------------------------------------------")
+
+    process_file(
+        input_file = dest, output_file = dest, words = notes, pages = None, action = None
+    )
 
 def extract_info(input_file: str):
     """
@@ -55,28 +57,10 @@ def search_for_text(lines, search_str):
     for line in lines:
         # Find all matches within one line
         results = re.findall(search_str, line, re.UNICODE)
-        # results = []
-        # found = False
-        # temp = ""
-        # for c in line:
-        #     if ord(c) == ord(search_str):
-        #         found = True
-        #         results.append(c)
-        #         # temp += c
-        #     elif found and (c == "."):
-        #         # temp += c
-        #         results.append(c)
-        #     else:
-        #         found = False
-        #         # if temp:
-        #         #     results.append(temp)
-        # # In case multiple matches within one line
-        # # print("line:", i, "result:",results)
-        # i += 1
         for result in results:
             yield result
             
-def highlight_matching_data(page, matched_values, type):
+def highlight_matching_data(page, matched_values, color):
     """
     Highlight matching values
     """
@@ -85,38 +69,22 @@ def highlight_matching_data(page, matched_values, type):
     for val in matched_values:
         matches_found += 1
         matching_val_area = page.get_text("words",sort=False)
-        print(val)
-        print(matching_val_area)
+        print(color)
         for word in matching_val_area:
             if val in word[4]:
                 rect_comp = create_rectangle(word, val)
                 # rect_comp = fitz.Rect(word[0],word[1],word[2],word[3])
                 highlight = page.add_highlight_annot(rect_comp)
+                if color == 1:
+                    highlight.set_colors(colors=fitz.utils.getColor('green'))
+                elif color == 2:
+                    highlight.set_colors(colors=fitz.utils.getColor({}))
+                elif color == 3:
+                    highlight.set_colors(colors=fitz.utils.getColor('pink'))
                 highlight.update()
-        # matching_val_area = page.search_for(val)
-        # for coordinate in matching_val_area:
-        #     if not isExactMatch(page, val, coordinate, False, True):
-        #         matching_val_area.remove(coordinate)
-        # print("matching_val_area",matching_val_area)
-        # highlight = None
-        # if type == 'Highlight':
-        #     highlight = page.add_highlight_annot(matching_val_area)
-        # elif type == 'Squiggly':
-        #     highlight = page.add_squiggly_annot(matching_val_area)
-        # elif type == 'Underline':
-        #     highlight = page.add_underline_annot(matching_val_area)
-        # elif type == 'Strikeout':
-        #     highlight = page.add_strikeout_annot(matching_val_area)
-        # else:
-        #     highlight = page.add_highlight_annot(matching_val_area)
-        # # To change the highlight colar
-        # # highlight.setColors({"stroke":(0,0,1),"fill":(0.75,0.8,0.95) })
-        # # highlight.setColors(stroke = fitz.utils.getColor('white'), fill = fitz.utils.getColor('red'))
-        # # highlight.setColors(colors= fitz.utils.getColor('red'))
-        # highlight.update()
     return matches_found
 
-def process_data(input_file: str, output_file: str, search_str: str, pages: Tuple = None, action: str = 'Highlight'):
+def process_data(input_file: str, output_file: str, words, pages: Tuple = None, action: str = 'Highlight'):
     """
     Process the pages of the PDF File
     """
@@ -136,13 +104,18 @@ def process_data(input_file: str, output_file: str, search_str: str, pages: Tupl
         # Get Matching Data
         # Split page by lines
         page_lines = page.get_text("text").split('\n')
-        matched_values = search_for_text(page_lines, search_str)
-        if matched_values:
-            matches_found = highlight_matching_data(
-                page, matched_values, 'Highlight')
-            total_matches += matches_found
-    print(f"{total_matches} Match(es) Found of Search String {search_str} In Input File: {input_file}")
+        counter = -1
+        for word in words:
+            matched_values = search_for_text(page_lines, word)
+            counter += 1
+            if matched_values:
+                print(counter)
+                matches_found = highlight_matching_data(
+                    page, matched_values, counter)
+                total_matches += matches_found
+            # print(f"{total_matches} Match(es) Found of Search String {word} In Input File: {input_file}")
     # Save to output
+    print(f"Highlighting done! File can be found in {input_file}")
     pdfDoc.save(output_buffer)
     pdfDoc.close()
     # Save the output buffer to the output file
@@ -159,43 +132,11 @@ def process_file(**kwargs):
     output_file = kwargs.get('output_file')
     if output_file is None:
         output_file = input_file
-    search_str = kwargs.get('search_str')
+    words = kwargs.get('words')
     pages = kwargs.get('pages')
-    # Redact, Frame, Highlight, Squiggly, Underline, Strikeout, Remove
     action = kwargs.get('action')
-    if action == "Remove":
-        # Remove the Highlights except Redactions
-        remove_highlght(input_file=input_file,
-                        output_file=output_file, pages=pages)
-    else:
-        process_data(input_file=input_file, output_file=output_file,
-                     search_str=search_str, pages=pages, action=action)
-
-def process_folder(**kwargs):
-    """
-    Redact, Frame, Highlight... all PDF Files within a specified path
-    Remove Highlights from all PDF Files within a specified path
-    """
-    input_folder = kwargs.get('input_folder')
-    search_str = kwargs.get('search_str')
-    # Run in recursive mode
-    recursive = kwargs.get('recursive')
-    #Redact, Frame, Highlight, Squiggly, Underline, Strikeout, Remove
-    action = kwargs.get('action')
-    pages = kwargs.get('pages')
-    # Loop though the files within the input folder.
-    for foldername, dirs, filenames in os.walk(input_folder):
-        for filename in filenames:
-            # Check if pdf file
-            if not filename.endswith('.pdf'):
-                continue
-             # PDF File found
-            inp_pdf_file = os.path.join(foldername, filename)
-            print("Processing file =", inp_pdf_file)
-            process_file(input_file=inp_pdf_file, output_file=None,
-                         search_str=search_str, action=action, pages=pages)
-        if not recursive:
-            break
+    process_data(input_file=input_file, output_file=output_file,
+                    words=words, pages=pages, action=action)
 
 def is_valid_path(path):
     """
@@ -237,26 +178,6 @@ def parse_args():
     print("\n".join("{}:{}".format(i, j) for i, j in args.items()))
     print("######################################################################")
     return args
-
-def isExactMatch(page, term, clip, fullMatch=False, caseSensitive=False):
-# clip is an item from page.search_for(term, quads=True)
-
-    termLen = len(term)
-    termBboxLen = max(clip.height, clip.width)
-    termfontSize = termBboxLen/termLen
-    f = termfontSize*2
-
-    # clip = clip.rect
-
-    validate = page.get_text("blocks", clip = clip + (-f, -f, f, f), flags=0)[0][4]
-    flag = 0
-    if not caseSensitive:
-        flag = re.IGNORECASE
-
-    matches = len(re.findall(f'{term}', validate, flags=flag)) > 0
-    if fullMatch:
-        matches = len(re.findall(f'\\b{term}\\b', validate))>0
-    return matches
 
 def create_rectangle(word, val):
     text = word[4]
